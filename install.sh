87880edfcadd3e09b3efce7245fb5ae31b326e5a
Briano Goestiawan <b@briano.io>
2021-07-11 02:20:50 +1000

    fix install script

--- install.sh
+++ install.sh
@@ -1,25 +1,19 @@
-set -e      # Exit on error
+set -e      # Exit on error.
 
 # Set key repeat timing.
-defaults write -g InitialKeyRepeat -int 15  # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1          # normal minimum is 2 (30 ms)
+defaults write -g InitialKeyRepeat -int 15      # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1              # normal minimum is 2 (30 ms)
 
-# Disable annoying popup "<app> quit unexpectedly"
+# Disable annoying popup "<app> quit unexpectedly".
 defaults write com.apple.CrashReporter DialogType none
 
 # Source Moom.
-defaults import com.manytricks.Moom moom/Moom.plist
+defaults import com.manytricks.Moom Moom.plist
 
-# Install Homebrew formulas.
-bash -c "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-for formula in $(cat brew/leaves); do
-    brew install "$formula"
-done
-for cask in $(cat brew/casks); do
-    brew install "$cask"
-done
+# Install formulae in Brewfile.
+brew bundle
 
-# Create symlinks.
+# Create symbolic links.
 stow bin cmus git karabiner prettier tmux vim zsh
 
 echo 'Restart for some settings (key repeat) to apply'
