b62fddb3a97c3ab0a1c4c307712c9453f312db1e
Briano Goestiawan <b@briano.io>
2021-08-05 02:00:27 +1000

    terminfo: add tmux-256color

--- install.sh
+++ install.sh
@@ -2,33 +2,34 @@
 
 set -e
 
-tic terminfo
+rm -rf ~/.terminfo
+tic tmux-256color.terminfo
 
-defaults write -g InitialKeyRepeat -int 14                  # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1                          # normal minimum is 2 (30 ms)
-defaults write com.apple.CrashReporter DialogType none      # Disable annoying popup "<app> quit unexpectedly"
+defaults write -g InitialKeyRepeat -int 14			# normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1				# normal minimum is 2 (30 ms)
+defaults write com.apple.CrashReporter DialogType none		# Disable annoying popup "<app> quit unexpectedly"
 defaults import com.manytricks.Moom Moom.plist
 
 # Homebrew
 if [ ! -d /usr/local/Homebrew ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 brew bundle install
 
 # Tmux TPM
 if [ ! -d ~/.tmux/plugins/tpm ]; then
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 # Set symbolic links
 for f in $(ls -a | grep '^\.' | grep -v '^\.\.?$' | grep -v '^\.(git|gitignore)$'); do
-    rm -rf ~/$f
-    ln -s ~/dotfiles/$f ~/$f
+	rm -rf ~/$f
+	ln -s ~/dotfiles/$f ~/$f
 done
 
 # Link bin directory
 rm -rf ~/bin; mkdir ~/bin
 cd bin
 for f in *; do
-    ln -s ~/dotfiles/bin/$f ~/bin/$f
+	ln -s ~/dotfiles/bin/$f ~/bin/$f
 done
