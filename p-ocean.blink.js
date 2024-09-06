fd83e6349e1b2eed77029049810b8e912da3d3fc
Briano Goestiawan <b@briano.io>
2024-09-06 19:53:19 +1000

    spring cleaning

--- p-ocean.blink.js
+++
@@ -1,7 +0,0 @@
-t.prefs_.set("color-palette-overrides", [
-	"#2b303c", "#ec5e66", "#99c793", "#fac862", "#6699cb", "#c78eb0", "#5fb3b2", "#d4d4d4",
-	"#566178", "#f0868c", "#b2d5ae", "#fbd589", "#8cb2d8", "#d5aac4", "#87c6c5", "#eeeeee",
-])
-t.prefs_.set("foreground-color", "#d4d4d4")
-t.prefs_.set("background-color", "#0e1014")
-t.prefs_.set("cursor-color", "#6a26ff")
